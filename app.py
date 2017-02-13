04703ea427923b7ff25fc991cc1a1e69c4209095
Amey Kusurkar <amey1000@gmail.com>
2017-02-13 18:51:45 +0000

    Implement sessions

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template, g, request, redirect
+from flask import Flask, render_template, g, request, redirect, session
 from db_utils import get_db, close_db
 import json
 
 app = Flask(__name__)
+app.secret_key = 'hakuna matata'
 
 @app.route('/')
 def index():
-    db = get_db(g)
-    cur = db.execute('select * from entries order by id desc')
-    return render_template('index.html', entries=cur.fetchall())
+    username = 'Nobody'
+    if 'username' in session:
+        username = session['username']
+        db = get_db(g)
+        cur = db.execute('select * from entries where username=? order by id desc', [username])
+        return render_template('index.html', entries=cur.fetchall(), username=username)
+    else:
+        return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect('/')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_entry():
+    if 'username' not in session:
+        return redirect('/login')
     if request.method == 'POST':
         db = get_db(g)
-        db.execute('insert into entries (key, value) values (?, ?)',
-                   [request.form['key'], request.form['value']])
+        db.execute('insert into entries (username, key, value) values (?, ?, ?)',
+                   [session['username'], request.form['key'], request.form['value']])
         db.commit()
         return redirect('/')
     else:
--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
+  username text not null,
   key text not null,
   value text not null
 );
